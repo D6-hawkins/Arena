baf06db6c18a7406cd46c67ccaa4dc89fb90cdf3
Dominic Hawkins <dominic2.hawkins@live.uwe.ac.uk>
2017-05-24 15:43:46 +0100

    Initial commit

---
+++ Assets/Selfmade/ColourChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChanger : MonoBehaviour {
+    float Colour = 0.0f;
+    float PrevColour = 0.0f;
+	// Use this for initialization
+	void Start () {
+       // print("Colour started");
+        GetComponent<Renderer>().material.color = new Color(Colour, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Colour != PrevColour)
+        {
+           // print(Colour);
+            GetComponent<Renderer>().material.color = new Color(Colour, 0, 0);
+            PrevColour = Colour;
+        }
+	}
+    void OnTriggerStay(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Colour += 0.01f;
+        }
+        if (Colour > 1.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Selfmade/KillSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillSwitch : MonoBehaviour {
+    // Use this for initialization
+    //public GameObject player1;
+	void Start () {
+        print("start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider Player)
+    {
+        if (Player.gameObject.name == "Player1" || Player.gameObject.name == "Player2")
+        {
+            print(Player.name);
+            print("Is dead.");
+            Destroy(Player.gameObject);
+        }
+    }
+}
